103a17ceaf52af3a3e39c79c5bc07180f562766b
e2saeki <saeki@e-2.co.jp>
2020-05-16 16:45:19 +0900

    searchBUTTON調整

--- src/givelog-plus/webroot/js/QuerySearchContent.js
+++ src/givelog-plus/webroot/js/QuerySearchContent.js
@@ -1,11 +1,10 @@
 const vueSearch = {
     template: `
     <div class="search-wrap" v-bind:class="{ 'show': isShow }" >
-    <a id="search" class="fas fa-search" v-bind:class="{ 'show': isShow }" @click="showToggle()"></a>
+        <div id="search" class="fas fa-search" v-bind:class="{ 'show': isShow }" @click="showToggle()"></div>
         <div v-bind:class="{ 'show': isShow }">
             <slot name="body"></slot>
         </div>
-
     </div>
     `,
     data() {
